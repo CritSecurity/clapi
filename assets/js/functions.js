6708f20c86aad0c13c874978b43f4436b61837b3
PossumInABox <34094714+PossumInABox@users.noreply.github.com>
2022-02-22 16:50:59 +0100

    add export to file function

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -24,3 +24,21 @@ function displaySpinner() {
 function hideSpinner() {
     document.querySelector("#loadingIndicator").innerHTML = ""
 }
+
+function download(data, filename, type) {
+    let file = new Blob([data], { type: type });
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        let a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
